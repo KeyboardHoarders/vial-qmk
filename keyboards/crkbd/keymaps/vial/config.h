ba601481d018c83a3cbdc03df386e8725029f6f5
slingthat <jdingerlee@gmail.com>
2024-05-26 14:55:26 -0500

    xe

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -73,10 +73,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    undef RGBLED_SPLIT
 #    define RGBLED_SPLIT \
         { 27, 27 }
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
 #    define RGBLIGHT_LIMIT_VAL 120
 #    define RGBLIGHT_HUE_STEP  10
 #    define RGBLIGHT_SAT_STEP  17
 #    define RGBLIGHT_VAL_STEP  17
+
+#    define RGB_MATRIX_STARTUP_HUE 35
+#    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
